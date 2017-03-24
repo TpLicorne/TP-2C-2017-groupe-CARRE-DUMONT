ccc2f343c7d16c1be1bacb9efb5cd47d2a2fb716
Aria547 <aur.dum@hotmail.fr>
2017-03-24 12:34:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-/** Systeme de 5 journee**/
-
+const EventEmitter = require('events').EventEmitter;
 const {Spiderman} = require('./app/spiderman');
 const {Deadpool} = require('./app/deadpool');
 const {Pony} = require('./app/poney');
 const {Unicorn} = require('./app/licorne');
 
 
-var EventEmitter = require('events').EventEmitter;
-var i;
+let i;
+const s = new Spiderman();
+const d = new Deadpool();
+const p = new Pony();
+const u = new Unicorn();
 
-
-const s = new Spiderman;
-const d = new Deadpool;
-const p = new Pony;
-const u = new Unicorn;
-
-
-for (i=0;i<=5;i++) {
-
-    var day = new EventEmitter();
-    day.on('day', () => console.log('jour'));
-    day.on('night', () => console.log('nuit'));
-
-
-    day.emit('day');
-    console.log ("");
-
-    d.WinLife(u,p);
-    s.do(u,p);
-    d.transform(p,u,s);
-
-    console.log ("");
-
-    day.emit('night');
-
-    console.log ("");
-    d.LostLife();
-    p.WinEnergy();
-    console.log ("");
+for (i = 0; i <= 5; i++) {
+  const day = new EventEmitter();
+  day.on('day', () => console.log('jour'));
+  day.on('night', () => console.log('nuit'));
+  day.emit('day');
+  console.log('');
+  d.winLife(u, p);
+  s.do(u, p);
+  d.transform(p, u, s);
+  console.log('');
+  day.emit('night');
+  console.log('');
+  d.lostLife();
+  p.winEnergy();
+  console.log('');
 }
-
 
